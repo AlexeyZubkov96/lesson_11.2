0f16b37ff12af6a1f8092fd41bd81c57aefcb62a
alexey_zubkov <zubkov.superstar@yandex.ru>
2024-05-26 21:20:21 +0300

    Добавлена новая функция в файл str_func.py

--- str_func.py
+++ str_func.py
@@ -1,7 +1,12 @@
 def capital_letter(line):
     """
-    Возвращает строку с большой буквы
+    Строка состоящая из первой заглавной буквы
     """
     return line.title()
 
+def lagre_letters(line):
+    """
+    Строка состоящяя из всех заглавных букв
+    """
+    return line.upper()
 
